10d84cc91369461c538a6fccbd980990af4cd0a0
unknown <lucho27ris@gmail.com>
2023-06-06 16:18:25 -0300

    varios navbar

--- src/components/Navbar/Navbar.vue
+++ src/components/Navbar/Navbar.vue
@@ -1,16 +1,17 @@
 <template>
-    <div class="bg-gray-900 text-gray-100 py-3 5 px-6 shadow md:flex justify-between items-center">
+    <div class="bg-slate-50 text-purple-900 py-3 5 px-6 shadow-xl md:flex justify-between items-center shadow-xl">
         <div class="flex items-center cursor-pointer">
-            <router-link @click="MenuClose()" class="text-xl" to="/">Sportify!</router-link>
+            <router-link @click="MenuClose()" class="text-xl font-bold" to="/">Sportify!</router-link>
         </div>
 
         <span @click="MenuOpen()" class="absolute md:hidden right-6 top-1.5 cursor-pointer text-4xl">
             <i :class="[open ? 'bi bi-x' : 'bi bi-list']"></i>
         </span>
-
-        <ul class="md:flex md:items-center md:px-0 px-10 md:pb-0 pb-15 md:static absolute bg-gray-900 md:w-auto w-full top-10 duration-700 ease-in" :class="[open ? 'left-0' : 'left-[-100%]']">
+        <ul class="md:shadow-none shadow-xl md:flex md:items-center md:px-0 px-10 md:pb-0 pb-15 bg-slate-50 md:static absolute md:w-auto w-full top-10 duration-700 ease-in" :class="[open ? 'left-0' : 'left-[-100%]']">
             <li class="md:mx-4 md:my-0 my-6" v-for="link in links" :key="link.key">
-                <router-link @click="MenuClose()" class="text-xl hover:text-green-500" :to="link.link">{{ link.name }}</router-link>
+                <router-link @click="MenuClose()" class="text-xl hover:text-teal-500 font-semibold" :to="link.link">
+                    {{ link.name }}
+                </router-link>
             </li>
         </ul>
     </div>
