05d495e83b17574def4b4cb27430dae31d439d49
Andrés <andres@DESKTOP-0DODEQP.localdomain>
2023-06-07 10:17:06 -0300

    login primera version

---
+++ src/views/LoginView.vue
@@ -0,0 +1,15 @@
+<template>
+    <Login />
+  </template>
+  
+  <script>
+    import Login from '@/components/Sesion/Login.vue'
+  
+  export default {
+    name: 'Home',
+    components: {
+      Login
+    }
+  }
+  </script>
+  
