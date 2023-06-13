61b55dfa90ded8baec04546b28e644ab8ae8161e
Andrés <andres@DESKTOP-0DODEQP.localdomain>
2023-06-13 15:41:34 -0300

    router-link agregado

--- src/components/Team/TeamCard.vue
+++ src/components/Team/TeamCard.vue
@@ -1,7 +1,7 @@
 <template>  
     <div class="flex align-center justify-center">
         <div class="min-w-xs max-w-md h-60 p-6 bg-white border border-gray-200 rounded-lg shadow flex flex-col justify-between hover:bg-gray-200 hover:border hover:border-gray-200">
-            <a href="/team">
+            <router-link to="/team">
                 <div class="justify-between mb-[15px] md:mb-0 md:flex ">
                     <div class="flex justify-start">
                         <img class="max-w-[10%] max-h[10%] mb-5 mr-2" src="@/assets/team.png" alt="image description">
@@ -15,7 +15,7 @@
                 </div>
                 <p class="mb-3 font-normal text-gray-500">Equipo del turno mañana de los días martes y jueves.</p>
                 <p class="mb-3 font-normal text -md text-gray-500">6/9/2023</p>
-            </a>
+        </router-link>
         </div>
     </div>
 </template>
